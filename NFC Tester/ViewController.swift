60eb62e5413d5b70091e745fc7401d92bab9963e
Bruno Philipe <brunophilipe@me.com>
2017-06-06 22:51:47 +0200

    Initial Commit

---
+++ NFC Tester/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  NFC Tester
+//
+//  Created by Bruno Philipe on 6/6/17.
+//  Copyright © 2017 Bruno Philipe. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view, typically from a nib.
+	}
+
+	override func didReceiveMemoryWarning() {
+		super.didReceiveMemoryWarning()
+		// Dispose of any resources that can be recreated.
+	}
+
+
+}
+
